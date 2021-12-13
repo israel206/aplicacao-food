649bd263dab86184c8f31dccc3211d54a1624822
Israel Pereira <israel.pereira@dce.ufpb.br>
2021-12-13 00:19:54 -0300

    update

--- front/src/services/produtos.js
+++ front/src/services/produtos.js
@@ -1,11 +1,10 @@
 import {http} from './config'
-
+// configuração 
 export default {
 
     listar:() => {
         return http.get('products')
     },
-
 
     salvar:(produto) => {
         return http.post('products', produto )
